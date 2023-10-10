d06f37626b4099c6d98d3f85d8a4b834b028622d
EA - Yolanda Proensa Silva <yolanda_proensa@educadventista.org>
2023-10-10 20:26:06 -0300

    conectando mysql

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql2")
+
+const app = express()
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3306
+})
+
+conn.connect((error) => {
+    if(error){
+        console.log(error)
+        return
+    }
+
+    console.log("Conectado ao MySQL!")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000!")
+    })
+})
